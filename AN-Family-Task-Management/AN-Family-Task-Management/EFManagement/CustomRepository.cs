d9d5d15b1b36788fdebc0f9e6a9c5a23aa61093e
Agajan Nazarow <agajannazarow1999@gmail.com>
2022-04-14 04:58:23 +0300

    Added model classes and grid

---
+++ AN-Family-Task-Management/AN-Family-Task-Management/EFManagement/CustomRepository.cs
@@ -0,0 +1,21 @@
+﻿using AN_Family_Task_Management.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AN_Family_Task_Management.EFManagement
+{
+    public class CustomRepository
+    {
+        public List<FamilyPerson> GetFamilyPersons()
+        {
+            CustomDBContext customDBContext = new CustomDBContext();
+            return customDBContext.FamilyPersons.ToList();
+        }
+
+        public List<Task> GetTasks()
+        {
+            CustomDBContext customDBContext = new CustomDBContext();
+            return customDBContext.Tasks.ToList();
+        }
+    }
+}
---
+++ AN-Family-Task-Management/AN-Family-Task-Management/Models/FamilyPerson.cs
@@ -0,0 +1,9 @@
+﻿namespace AN_Family_Task_Management.Models
+{
+    public class FamilyPerson
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
